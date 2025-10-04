92e24cb92def6c70f829dd9e795d6862146e89d7
hophuoc4so9 <2224802010872@student.tdmu.edu.vn>
2025-10-04 14:11:58 +0700

    new

---
+++ example301/src/main/java/com/nguyenanhtu/example303/Example303Application.java
@@ -0,0 +1,13 @@
+package com.nguyenanhtu.example303;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Example303Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Example303Application.class, args);
+	}
+
+}
